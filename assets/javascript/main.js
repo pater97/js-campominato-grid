ed0a91a6aab296ace929e41d8f384f46a162e3f7
pater97 <giuseppepater997@gmail.com>
2021-10-20 14:27:29 +0200

    impostazioni iniziali

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+//CONSEGNA
+/*
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+*/
+
+//chiedere alll'utente attraverso un promp il livello di difficoltà
